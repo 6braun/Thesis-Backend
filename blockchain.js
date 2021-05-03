084df595028ec07942eb1bed987ccf7b288940a4
6braun <danielb129@live.de>
2021-05-04 01:44:59 +0200

    wip

--- blockchain.js
+++ blockchain.js
@@ -1,9 +1,7 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const { ethers } = require('ethers')
+const {ethers} = require('ethers')
+const fs = require('fs');
 
 const abi = require('./abis/AdvertContract.json');
-
-console.log(abi);
 
 
 const mnemonic = '0xdaa62747cf7bd46238ac388bca529ae8d85b2dbcf32ad4bcdb7f6477e991f60d';
@@ -25,18 +23,36 @@ const ABI = ['function receiveEther() payable public',
 const contract = new ethers.Contract(abi.networks['3'].address, ABI, provider)
 const signedContract = contract.connect(signer);
 
-contract.getBalance().then(res => console.log(parseInt(res, 16)));
+// contract.getBalance().then(res => console.log(parseInt(res, 16)));
 
 function sendMoney() {
 
-    // Fix von ricmoo @ Github
     const dai = ethers.utils.parseEther('0.1');
+
+    // Fix von ricmoo @ Github
     const overrides = {
         value: dai
     };
     signedContract.receiveEther(overrides).then(res => console.log(res));
 }
 
+function getBalance() {
+    contract.getBalance().then(res => console.log(parseInt(res, 16)));
+}
 
+function getIndex() {
+    var files = fs.readdirSync('./uploads/');
+    let index;
+    files = files.map(el => {
+        return parseInt(el.substring(el.indexOf('_') + 1, el.length))
+            ;
+    });
+    index = files.reduce((a, b) => Math.max(a, b));
+    return index;
+}
 
-// sendMoney();
+function getSigner() {
+    return signer;
+}
+
+module.exports = {getIndex, getSigner, sendMoney, getBalance};
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 // Erstes minimales Backend
-path = require('path');
+const path = require('path');
+const fs = require('fs');
+
+const blockchain = require('./blockchain');
+
+// const index = blockchain.getIndex();
 
 const bodyparser = require("body-parser");
 const cors = require("cors");
@@ -10,8 +15,7 @@ const express = require("express");
 // Ist eine Middleware zur Behandlung von multipart/form-data,
 // also dem Upload aus dem Frontend
 const multer = require("multer");
-// API für die Ethereum-Blockchain
-const web3 = require("web3")
+
 
 const app = express();
 
@@ -20,30 +24,8 @@ app.use(cors())
 app.use(bodyparser.json());
 
 
-const storage = multer.diskStorage({
-
-    // Setzen des Verzeichnis für die Uploads
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-
-    // Namen der hochgeladenen Datei setzen
-    filename: function (req, file, cb) {
-        console.log(file);
-        cb(null, file.originalname);
-    }
-})
-
 var upload = multer({
-    storage: storage,
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.jpg$/)) {
-            //Error
-            cb(new Error('Nur jpgs dürfen hochgeladen werden'));
-        }
-        //Success
-        cb(undefined, true);
-    }
+    dest: "./uploads"
 })
 // GETTER-METHODE FÜR DAS BELUGA-BILD
 app.get('/beluga', (req, res) => {
@@ -59,22 +41,68 @@ app.get('/foxes', (req, res) => {
     //res.end();
 });
 // POST-REQUEST, MIT DER DATEIEN HOCHGELADEN WERDEN KÖNNEN (SIEHE /uploads)
-app.post('/upload', upload.single('uploadedImage'), (req, res, next) => {
-    const file = req.file;
-    if (!file) {
-        const error = new Error('Datei ungültig')
-        error.httpStatusCode = 400
-        return next(error)
+app.post('/upload', upload.single('uploadedImage'), (req, res) => {
+    const tempPath = req.file.path;
+    const index = getIndex();
+    const originalFilename = req.file.originalname.substring(0, req.file.originalname.indexOf('.'));
+    const filename = originalFilename + index + '.jpg';
+    const targetPath = path.join(__dirname, "./uploads/" + filename);
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".jpeg") {
+        fs.rename(tempPath, targetPath, err => {
+            if (err) return handleError(err, res);
+
+            res
+                .status(200)
+                .contentType("text/plain")
+                .end("File uploaded!");
+        });
+
+        console.log(parseInt(req.body['etherSend']));
+    } else {
+        fs.unlink(tempPath, err => {
+            if (err) return handleError(err, res);
+
+            res
+                .status(403)
+                .contentType("text/plain")
+                .end("Only .jpeg files are allowed!");
+        });
     }
-    res.status(200).send({
-        statusCode: 200,
-        status: 'success',
-        uploadedFile: file
-    })
-}, (error, req, res) => {
-    res.status(400).send({
-        error: error.message
-    })
 })
 
 app.listen(3000, () => console.log('Listening'))
+
+
+function getIndex() {
+    var files = fs.readdirSync('./uploads/');
+
+    if (files.length === 1) {
+        return '_1';
+    } else {
+        return getCurrentFileIndexForName(files);
+    }
+}
+
+
+function getCurrentFileIndexForName(_files) {
+
+    let index;
+    let files = _files.filter(el => {
+        return el.toString().includes('_');
+    })
+    files = files.map(el => {
+        return parseInt(el.substring(el.lastIndexOf('_') + 1, el.length))
+            ;
+    });
+    index = files.reduce((a, b) => Math.max(a, b));
+
+    index += 1
+    return '_' + index;
+}
+
+function getIndexAsNumber(_index) {
+    let index = _index.substring(_index.indexOf('_') + 1, _index.length);
+    return parseInt(index);
+}
+
