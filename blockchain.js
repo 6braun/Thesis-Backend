88281fce1de90861e4aa8a33722f389ad0ac0419
6braun <danielb129@live.de>
2021-05-04 23:45:58 +0200

    Rohversion fertiggestellt

--- blockchain.js
+++ blockchain.js
@@ -19,8 +19,9 @@ ABI = [
     'function receiveEther() payable public',
     'function getBalance() public view returns (uint)',
     'function withdrawEtherTo(address payable _to) public',
-    'function receiveNewAd(uint _id, uint _wei) payable public',
-    'function getFunds(uint _id) public view returns(uint)'];
+    'function augmentAds(uint _id, uint _wei) payable public',
+    'function getFunds(uint _id) public view returns(uint)',
+    'function showAd(uint _id) public'];
 
 
 const contract = new ethers.Contract(abi.networks['3'].address, ABI, provider)
@@ -28,44 +29,39 @@ const signedContract = contract.connect(signer);
 
 // contract.getBalance().then(res => console.log(parseInt(res, 16)));
 
-function sendMoney() {
-
-    const dai = ethers.utils.parseEther('0.1');
-
-    // Fix von ricmoo @ Github
-    const overrides = {
-        value: dai
-    };
-    signedContract.receiveEther(overrides).then(res => console.log(res));
-}
+// function sendMoney() {
+//
+//
+//     const dai = ethers.utils.parseEther('0.1');
+//
+//     // Fix von ricmoo @ Github
+//     const overrides = {
+//         value: dai
+//     };
+//     signedContract.receiveEther(overrides).then(res => console.log(res));
+// }
 
 function newAd(body) {
-    return signedContract.receiveNewAd(body.id, body.funds);
+    console.log(signedContract);
+    return signedContract.augmentAds(body.id, body.funds);
 }
 
 function getFunds(_id) {
     signedContract.getFunds(_id).then(res => console.log(res));
 }
 
-function getBalance() {
-    contract.getBalance().then(res => console.log(parseInt(res, 16)));
-}
-
-function getIndex() {
-    var files = fs.readdirSync('./uploads/');
-    let index;
-    files = files.map(el => {
-        return parseInt(el.substring(el.indexOf('_') + 1, el.length))
-            ;
-    });
-    index = files.reduce((a, b) => Math.max(a, b));
-    return index;
-}
+// function getBalance() {
+//     contract.getBalance().then(res => console.log(parseInt(res, 16)));
+// }
 
 function getSigner() {
     return signer;
 }
 
-module.exports = {getIndex, getSigner, sendMoney, getBalance, newAd};
+function showAd(id){
+    return signedContract.showAd(id);
+}
+
+module.exports = {newAd, showAd};
 
 
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 // Erstes minimales Backend
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
+
 
 const blockchain = require('./blockchain');
 
@@ -28,18 +30,29 @@ var upload = multer({
     dest: "./uploads"
 })
 // GETTER-METHODE FÜR DAS BELUGA-BILD
-app.get('/beluga', (req, res) => {
-    const filePath = path.join(__dirname, './images/beluga.jpg');
+app.get('/getImage', (req, res) => {
+    const fileToHost = getRandomFile();
+    if(fileToHost){
+    const filePath = path.join(__dirname, './uploads/' + fileToHost);
     res.sendFile(filePath);
-    //res.end();
+
+    const index = getIndexOfFile(fileToHost)
+    blockchain.showAd(index).then(res => {
+        console.log('Ad Shown');
+    }).catch(err => {
+        console.log('no Funds left');
+    })}
+    else{
+        res.sendStatus(400);
+    }
 });
 
-// GETTER-METHODE FÜR DAS FUCHS-BILD
-app.get('/foxes', (req, res) => {
-    const filePath = path.join(__dirname, './images/foxes.jpg');
-    res.sendFile(filePath);
-    //res.end();
-});
+function getRandomFile() {
+    const files = fs.readdirSync('./uploads/');
+    return _.sampleSize(files, 1)[0];
+}
+
+
 // POST-REQUEST, MIT DER DATEIEN HOCHGELADEN WERDEN KÖNNEN (SIEHE /uploads)
 app.post('/upload', upload.single('uploadedImage'), (req, res) => {
     const tempPath = req.file.path;
@@ -61,12 +74,10 @@ app.post('/upload', upload.single('uploadedImage'), (req, res) => {
 
         const newAd = {
             id: getIndexAsNumber(index),
-            funds: parseFloat(req.body['etherSend']) * (10**18)
+            // Funds werden von Ether in Gwei umgewandelt
+            funds: parseFloat(req.body['etherSend']) * (10 ** 9)
         };
-
-        blockchain.newAd(newAd).then(res => console.log(res));
-
-        console.log(newAd);
+        blockchain.newAd(newAd).then(res => console.log(res)).catch(err => console.log(err));
 
     } else {
         fs.unlink(tempPath, err => {
@@ -80,7 +91,7 @@ app.post('/upload', upload.single('uploadedImage'), (req, res) => {
     }
 })
 
-app.listen(3000, () => console.log('Listening'))
+app.listen(3000, () => console.log('Listening'));
 
 
 function getIndex() {
@@ -108,6 +119,11 @@ function getCurrentFileIndexForName(_files) {
 
     index += 1
     return '_' + index;
+}
+
+function getIndexOfFile(file) {
+    const index = file.substring(file.lastIndexOf('_') + 1, file.indexOf('.'));
+    return parseInt(index);
 }
 
 function getIndexAsNumber(_index) {
